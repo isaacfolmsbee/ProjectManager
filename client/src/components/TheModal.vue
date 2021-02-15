760f4633cc7eaa7b7a881589cae891f701ae58e8
Isaac Folmsbee <if@sent.com>
2021-02-14 22:13:24 -0500

    Fixed modal not closing on selecting new route

--- client/src/components/TheModal.vue
+++ client/src/components/TheModal.vue
@@ -3,10 +3,10 @@
 	<svg @click="$emit('changeTheme'); $emit('closeModal')" :class="{ 'text-gray-800': darkMode, 'text-gray-100': !darkMode }" class="absolute h-11 w-11 top-5 left-5 fill-current" viewBox="0 0 24 24"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142c3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg>
 	<svg @click="$emit('closeModal')" class="absolute w-11 h-11 top-5 right-5 text-gray-200 fill-current" viewBox="0 0 24 24"><path d="M19 3H5a2 2 0 0 0-2 2V19a2 2 0 0 0 2 2H19a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2m-3.4 14L12 13.4L8.4 17L7 15.6l3.6-3.6L7 8.4L8.4 7l3.6 3.6L15.6 7L17 8.4L13.4 12l3.6 3.6l-1.4 1.4z"></path></svg>
 	<div class="h-full flex flex-col items-center">
-		<router-link to="/" class="router-link mt-24">Dashboard</router-link>
-		<router-link to="/tickets" class="router-link">Tickets</router-link>
-		<router-link to="/manage-users" class="router-link">Manage Users</router-link>
-		<router-link to="/manage-projects" class="router-link">Manage Projects</router-link>
+		<span @click="$emit('closeModal')" class="mb-6 mt-24"><router-link to="/" class="router-link">Dashboard</router-link></span>
+		<span @click="$emit('closeModal')" class="mb-6"><router-link to="/tickets" class="router-link">Tickets</router-link></span>
+		<span @click="$emit('closeModal')" class="mb-6"><router-link to="/manage-users" class="router-link">Manage Users</router-link></span>
+		<span @click="$emit('closeModal')" class="mb-6"><router-link to="/manage-projects" class="router-link">Manage Projects</router-link></span>
 		<span v-if="JWT" @click="$emit('logout'); $emit('closeModal')" class="router-link logout">Logout</span>
 	</div>
 </div>
@@ -30,7 +30,7 @@ export default Vue.extend({
 </script>
 <style lang="postcss" scoped>
 .router-link {
-	@apply py-1 px-2 mb-4 text-3xl font-bold text-gray-50;
+	@apply py-1 px-2 text-3xl font-bold text-gray-50;
 }
 
 .router-link.logout {
